6dc15df3d1532e61268fdd3bd8061c2a33e3316e
lyndsclev <lyndsclev@gmail.com>
2022-02-03 15:25:48 -0500

    added js pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// show starting text and start button
+
+// on start button click, start timer and display first question with multiple choice answers
+
+// on answer button click: display result, display next question, if answer is wrong, deduct 10 seconds from timer
+
+// end quiz by either: timer run out or answered all questions
+
+// display score and ask user to enter initials to save score
+
+// save score in local storage
+
+// include button option to clear high score(s)
