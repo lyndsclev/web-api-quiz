51814029e242926b7a392482475a965d946b4f5e
lyndsclev <lyndsclev@gmail.com>
2022-02-05 16:53:03 -0500

    question and buttons populating to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,102 +1,58 @@
+// variables 
 
-var startCont = document.querySelector("#startCont");
 var startBtnEl = document.querySelector("#startBtnEl");
-var quizCont = document.querySelector("#quizCont");
+var startCont = document.querySelector("#startCont");
+var questCont = document.querySelector("#questCont");
 
-// timer global variables 
-
-var time = 75
+var time = 60;
 var timerEl = document.querySelector("#timerEl");
 var timer;
 
-// questions array 
+// timer 
 
-var questionsArr = [
-    {
-        question: "How many donuts are made in the US each year?", 
-        answers: {
-            a: "Over 10 billion",
-            b: "5",
-            c: "100,000",
-            d: "None of the above"
-        },
-        correctAns: "a" 
-    },
-    {
-        question: "How people living in America have the last name of DONUT or DOUGHNUT?", 
-        answers: {
-            a: "80",
-            b: "10",
-            c: "60,000",
-            d: "None of the above"
-        },
-        correctAns: "b" 
-    },
-    {
-        question: "Which US city has the most donut shops per person?", 
-        answers: {
-            a: "San Francisco",
-            b: "Boston",
-            c: "New York",
-            d: "None of the above"
-        },
-        correctAns: "b" 
-    },
-    {
-        question: "What did the Dutch used to call donuts?", 
-        answers: {
-            a: "Nun's Farts",
-            b: "Bagels",
-            c: "Olykoeks",
-            d: "None of the above"
-        },
-        correctAns: "c" 
-    },
-];
+function clock () {
+    time--; 
+    timerEl.textContent = "TIME: " + time;
+}
 
-// on start button click, start timer
+// start
 
 function start () {
-    timer = setInterval(clock, 1000);
-    timerEl.textContent = time; 
 
-    // hide start content
+    // start timer
 
-    startCont.className = "hide";
+    timer - setInterval(clock, 1000);
+    
+    // remove start content
 
-    // un-hide quiz content 
+    startCont.remove();
 
-    quizCont.classList.remove("hide");
+    // question 1
 
-    // create quiz question text
+    var questOne = document.createElement("p");
+    questOne.textContent = "How many donuts are made in the US each year?";
+    questCont.appendChild(questOne);
 
-    // create quiz buttons
+    var ansOneArr = ["Over 10 billion", "5", "100,000", "None of the above"];
 
-    // loop through questions array 
+    for (var i = 0; i < ansOneArr.length; i++) {
+        var ansBtn = document.createElement("button")
+        ansBtn.innerHTML = ansOneArr[i];
+        questCont.appendChild(ansBtn);
+    };
 
+    // question 2
+
+    // question 3
+
+    // question 4
 }
 
 startBtnEl.addEventListener('click', start);
 
-function clock () {
+// end quiz 
 
-    time--;
-    timerEl.textContent = time;
-
-    if (time <= 0) {
-        endQuiz();
-
-    }
-}
-
-function endQuiz() {}
-
-
-// end quiz by either: timer run out or answered all questions
-
-// display score and ask user to enter initials to save score
-
-// save score in local storage
-
-// include button option to clear high score(s)
-
+// --- 
+// local storage
+// save score
+// high scores 
